1fbd4cf0f1cbe0fd3f340039c9f8df061bc6d59e
ADORELLANA96 <adkb.1409@gmail.com>
2024-01-24 00:35:58 -0500

    Creacion objetos Genericos Exception y Response

--- src/main/java/unir/store/products/dto/CategoryDTO.java
+++ src/main/java/unir/store/products/dto/CategoryDTO.java
@@ -1,15 +1,13 @@
 package unir.store.products.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
 public class CategoryDTO {
-	
-	private Long id;
-	private String name;
+
+        private final Long id;
+        private final String name;
 
 }
---
+++ src/main/java/unir/store/products/utils/StoreConstants.java
@@ -0,0 +1,8 @@
+package unir.store.products.utils;
+
+public abstract class StoreConstants {
+
+        public static final int FAILURE_CODE_DEFAULT = 1;
+        public static final String FAILURE_STATUS_DEFAULT = "ERROR";
+        public static final String STATUS_DEFAULT = "OK";
+}
